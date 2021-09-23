0c1114f74d4cf1b0309b941cb4abeb9d0f9a28be
ckost9n <ckost9n@gmail.com>
2021-09-23 15:04:04 +0300

    setup tasks vc

--- ToDoTestApp/Controller/TasksTableViewController.swift
+++ ToDoTestApp/Controller/TasksTableViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import RealmSwift
 
 class TasksTableViewController: UITableViewController {
     
     var currentTasksList: TasksList!
+    
+    var currentTasks: Results<Task>!
+    var completedTasks: Results<Task>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = currentTasksList.name
+        filteringTasks()
         
     }
     @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
@@ -28,22 +33,34 @@ class TasksTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        return section == 0 ? currentTasks.count : completedTasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "Current Tasks" : "Completed Tasks"
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "tasksCell", for: indexPath)
 
-        // Configure the cell...
+        var task: Task!
+        task = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
+        
+        cell.textLabel?.text = task.name
+        cell.detailTextLabel?.text = task.note
 
         return cell
+    }
+    
+    private func filteringTasks() {
+        currentTasks = currentTasksList.tasks.filter("isComplete = false")
+        completedTasks = currentTasksList.tasks.filter("isComplete = true")
+        tableView.reloadData()
     }
 
 }
